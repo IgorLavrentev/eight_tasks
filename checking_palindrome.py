1da74a50743b8e479fb4b0cdfda67d17befde022
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-05 21:53:38 +0300

    fifth correction checking_palindrome.py

--- checking_palindrome.py
+++ checking_palindrome.py
@@ -1,10 +1,17 @@
-def palindrome(string: str, n: int) -> bool:
+def palindrome(string: str) -> bool:
 
-    if len(string)//2 ==len(string) - n: # условие окончания рекурсии
+    # hasattr - функция, используемая для проверки наличия у объекта определенного атрибута
+    if not hasattr(palindrome, 'counter'): # если у функции palindrome нет атрибута counter
+        palindrome.counter = len(string)
+
+    if len(string)//2 == len(string) - palindrome.counter: # условие окончания рекурсии
+        # удаление атрибута функции для правильной работы функции при повторном вызове
+        del palindrome.counter
         return True
 
-    if string[len(string) - n] == string[n - 1]: # сравнивание первого и последнего элемента
-        n -= 1
-        return palindrome(string, n)
+    # сравнивание первого и последнего элемента
+    if string[len(string) - palindrome.counter] == string[palindrome.counter - 1]:
+        palindrome.counter -= 1
+        return palindrome(string)
 
     return False
