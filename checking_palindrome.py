0e008f04ab798aebc060224f0a1b43fc389d8bfd
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-06 21:50:31 +0300

    sixth correction checking_palindrome.py

--- checking_palindrome.py
+++ checking_palindrome.py
@@ -1,17 +1,15 @@
+# функция разворота строки
+def line_reversal(string_t: str) -> str:
+    if string_t == "":
+        return string_t
+    if string_t != "":
+        return line_reversal(string_t[1:]) + string_t[0]
+    # для согласования операторов return
+    return ''
+
 def palindrome(string: str) -> bool:
-
-    # hasattr - функция, используемая для проверки наличия у объекта определенного атрибута
-    if not hasattr(palindrome, 'counter'): # если у функции palindrome нет атрибута counter
-        palindrome.counter = len(string)
-
-    if len(string)//2 == len(string) - palindrome.counter: # условие окончания рекурсии
-        # удаление атрибута функции для правильной работы функции при повторном вызове
-        del palindrome.counter
+    string_reverse = line_reversal(string)
+    # сравнивание исхдной строки с развернутой
+    if string == string_reverse:
         return True
-
-    # сравнивание первого и последнего элемента
-    if string[len(string) - palindrome.counter] == string[palindrome.counter - 1]:
-        palindrome.counter -= 1
-        return palindrome(string)
-
     return False
