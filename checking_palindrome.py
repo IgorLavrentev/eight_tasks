4fc48474e3376a20668577e736302c716cdd6e0f
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-07 17:33:41 +0300

    eighth correction checking_palindrome.py

--- checking_palindrome.py
+++ checking_palindrome.py
@@ -3,15 +3,12 @@ def palindrome(string: str, n: int) -> bool:
 
     if len(string)//2 ==len(string) - n: # условие окончания рекурсии
         return True
-    if string[len(string) - n] == string[n - 1]: # сравнивание первого и последнего элемента
-        n -= 1
-        return palindrome(string, n)
-    return False
+    if string[len(string) - n] != string[n - 1]:
+        return False
+    return palindrome(string, n-1)
+
 
 # функция для получения исходных данных (строки)
 def accept(string: str) -> bool:
 
-    n = len(string) # длина строки
-    if palindrome(string, n):
-        return True
-    return False
+    return palindrome(string, len(string))
