c9eea8ae3a6d9fe7ee1926e312da3313e7e78267
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-08 22:14:42 +0300

    second correction even_indexes.py

--- even_indexes.py
+++ even_indexes.py
@@ -1,9 +1,9 @@
 # функция которая оставляет в списке только значения с четными индексами
 def even(list_original: list, counter: int) -> list:
     if counter < 1: # условие выхода из рекурсии
-        return list_original
-    if (counter - 1) % 2 != 0: # проверка индекса на нечетность
-        del list_original[counter - 1]
+        return None
+    if (counter - 1) % 2 == 0: # проверка индекса на четность
+        print(list_original[counter - 1])
     counter -= 1
     return even(list_original, counter)
 
