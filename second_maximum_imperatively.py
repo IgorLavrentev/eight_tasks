633baf9e934d71e277a7a9f9359c26422803cbd5
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-12 21:38:39 +0300

    second correction second_maximum_imperatively.py

--- second_maximum_imperatively.py
+++ second_maximum_imperatively.py
@@ -1,12 +1,11 @@
-# функция для нахождения второго максимального числа
 def second_maximum(initial_list: list) -> int:
-
-    # сортировка списка по убыванию
-    for i in range(len(initial_list)): # i проходит от 0 до длины списка
-        counter = i + 1  # при каждом проходе задается новое значение counter
-        for j in range(counter, len(initial_list)): # counter проходит от i + 1 до длины списка
-            # сравнение i-го элемента с индексом элемента 'counter'
-            if initial_list[i] < initial_list[j]:
-                initial_list[i],  initial_list[j] = initial_list[j], initial_list[i]
-
-    return initial_list[1] # в отсортированном списка возвращается втрой максимальный элемент
+    first_max = 0 # первое максимальное
+    second_max = 0 # второе максимальное
+    for i in range(len(initial_list)):
+        if initial_list[i] >= first_max:
+            second_max = first_max
+            first_max = initial_list[i]
+            continue
+        if initial_list[i] > second_max:
+            second_max = initial_list[i]
+    return second_max
