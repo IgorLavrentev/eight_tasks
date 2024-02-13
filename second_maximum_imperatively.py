b970065d501d24e8fc98134cb0a98bc12e584bb6
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-13 21:47:08 +0300

    fourth correction second_maximum_imperatively.py

--- second_maximum_imperatively.py
+++ second_maximum_imperatively.py
@@ -1,6 +1,10 @@
 def second_maximum(initial_list: list) -> int:
-    first_max = initial_list[0] # первое максимальное
-    second_max = initial_list[1] # второе максимальное
+    if initial_list[0] > initial_list[1]:
+        first_max = initial_list[0] # первое максимальное
+        second_max = initial_list[1] # второе максимальное
+    if initial_list[0] < initial_list[1]:
+        first_max = initial_list[1] # первое максимальное
+        second_max = initial_list[0] # второе максимальное
     for i in range(len(initial_list)):
         if initial_list[i] >= first_max:
             second_max = first_max
