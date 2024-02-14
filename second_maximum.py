b6d6b58ae5308adccba4a788968a1da08f26bf83
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-02-14 22:17:12 +0300

    fifth correction second_maximum.py

--- second_maximum.py
+++ second_maximum.py
@@ -17,7 +17,7 @@ def accept(initial_list: list) -> int:
     if initial_list[0] > initial_list[1]:
         first_max = initial_list[0] # первое максимальное
         second_max = initial_list[1] # второе максимальное
-    if initial_list[0] < initial_list[1]:
+    else:
         first_max = initial_list[1] # первое максимальное
         second_max = initial_list[0] # второе максимальное
     return second_maximum(initial_list, first_max, second_max, i)
